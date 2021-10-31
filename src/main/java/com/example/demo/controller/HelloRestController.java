006ef91fc17107e449653e0502fccfea9fdaab3a
shruthishruu05 <shruthi.m@perfios.com>
2021-10-31 17:01:38 +0530

    ADDED : Ability to demonstrate post method

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -2,10 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -24,4 +28,11 @@ public class HelloRestController {
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello "+name+" from BridgeLabz!";
 	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello "+user.getFirstName()+" "+user.getLastName()+"!";
+	}
+	
+	
 }
