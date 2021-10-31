4990251664555999ab6fd680ad00471ffab189f2
shruthishruu05 <shruthi.m@perfios.com>
2021-10-31 16:41:00 +0530

    ADDED : rest call to pass name as path variable

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloRestController {
 	@RequestMapping(value= {"","/","/home"})
 	public String sayHello() {
 		return "Hello from BridgeLabz!!";
+	}
+	
+	@RequestMapping(value= {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value="name") String name) {
+		return "Hello "+name+" from BridgeLabz!";
 	}
 }
